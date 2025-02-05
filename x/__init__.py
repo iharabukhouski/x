18304200c0a6a83c19b41de1767625b5a8e51eee
Ihar Abukhouski <ihar.abukhouski@gmail.com>
2025-02-04 22:06:16 -0600

    initial

--- x/__init__.py
+++ x/__init__.py
@@ -1,3 +1,4 @@
+import builtins
 import time
 from .env import env as _env
 from .date import date as _date
@@ -8,21 +9,7 @@ date = _date
 file = _file
 
 
-def sleep(
-    seconds=10000,
-):
-
-    time.sleep(seconds)
-
-
 _input = input
-
-
-def input(
-    str,
-):
-
-    return _input(f'{str} ')
 
 
 class range:
@@ -38,3 +25,31 @@ class range:
     def __next__(self):
 
         return self.i
+
+
+def _log(*args, **kvargs):
+
+    print(*args, **kvargs)
+
+
+builtins.log = _log
+
+
+def _sleep(
+    seconds=10000
+):
+
+    time.sleep(seconds)
+
+
+builtins.sleep = _sleep
+
+
+def _input(
+    str,
+):
+
+    return _input(f'{str} ')
+
+
+builtins.input = _input
